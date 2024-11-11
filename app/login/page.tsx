8a01e324cadf5e11df2bc8549148cafac1c22570
Caio Moraes <camoredev@gmail.com>
2024-11-10 22:03:30 -0300

    feat: add clark auth in page home

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function LoginPage() {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex max-w-[550px] flex-col justify-center p-8">
@@ -20,10 +28,13 @@ export default function page() {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant={"outline"}>
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+
+        <SignInButton>
+          <Button variant={"outline"}>
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
         <Image
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
 import Image from "next/image";
 import { Button } from "./_components/ui/button";
+import { SignOutButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <div>
-      <h1 className="text-green-500">Olá mundo!</h1>
-      <Button>hello world</Button>
+    <div className="flex h-full w-full flex-col">
+      <header className="flex h-16 w-full bg-gray-900 justify-between">
+        <Image
+          src={"/logo-home.svg"}
+          width={173}
+          height={39}
+          alt="logo"
+          className="p-4"
+        />
+        <ul className="flex gap-4">
+          <li>Dashboard</li>
+          <li>Transações</li>
+          <li>Assinatura</li>
+        </ul>
+        <div>caio moraes</div>
+      </header>
+      <main>
+        <h1 className="text-green-500">Olá mundo!</h1>
+        <SignOutButton>
+          <Button>Sair</Button>
+        </SignOutButton>
+      </main>
     </div>
   );
 }
