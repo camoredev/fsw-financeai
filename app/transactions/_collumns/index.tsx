63a855937f3f7607d3dabf9030f2e73e75c1212b
Caio Moraes <camoredev@gmail.com>
2024-11-15 19:47:56 -0300

    feat: formatted label transaction table rendered properly

--- app/transactions/_collumns/index.tsx
+++ app/transactions/_collumns/index.tsx
@@ -3,9 +3,30 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
+import { Edit, Trash2 } from "lucide-react";
+import { Button } from "@/app/_components/ui/button";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Habitação",
+  OTHER: "Outros",
+  SALARY: "SaLar10",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+};
+
+export const TRANSACTION_METHODS_LABELS = {
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  BANK_TRANSFER: "Transferência",
+  BANK_SLIP: "Boleto",
+  CASH: "Dinheiro",
+  PIX: "Pix",
+  OTHER: "Outro",
+};
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -22,21 +43,50 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_METHODS_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "value",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(+transaction.amount),
   },
   {
     accessorKey: "actions",
     header: "",
+    cell: () => {
+      return (
+        <div className="flex gap-6">
+          <Button variant={"ghost"} size={"icon"} className="text-muted">
+            {" "}
+            <Edit size={16} />
+          </Button>
+          <Button variant={"ghost"}>
+            {" "}
+            <Trash2 size={16} />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
